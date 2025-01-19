6d2148b8d5840d9eb7cfb493719e2e7671d7e9e0
Anirudh Gupta <aniamitgupta@gmail.com>
2025-01-19 15:23:29 +0530

    random arg

--- mac_changer.py
+++ mac_changer.py
@@ -7,26 +7,27 @@ def mac_changer(intf, new_mac):
     subprocess.call(["ifconfig", intf, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", intf, "up"])
     print(f"\n[+] Changing MAC Address of {intf} to {new_mac}\n")
-    new_mac_result(intf)
+    curr_mac_add(intf)
 
 def arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC Address")
     parser.add_option("-m", "--mac", dest="mac_address", help="New MAC Address")
-    # parser.add_option("-p", "--permanent", dest="permanent", help="Restore back to permanent address")
+    parser.add_option("-r", "--random", dest="random_mac", help="Random Mac Address")
+    
     (value, args) = parser.parse_args()
     if not value.interface:
         parser.error("[-] Please Specify Interface.")
     elif not value.mac_address:
-        parser.error("[-] Please Specify New Mac Address.")
+        parser.error("[-] Please Specify New MAC Address.")
     else:
         mac_changer(value.interface, value.mac_address)
 
-def new_mac_result(interface):
+def curr_mac_add(interface):
     ifc_r = subprocess.check_output(["ifconfig", interface])
     mac_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifc_r))
     if mac_result:
-        print("[+] New MAC Address is",mac_result.group(0))
+        return ("[+] New MAC Address is",mac_result.group(0))
     else:
         print("[-] Could not read MAC Address")
 
